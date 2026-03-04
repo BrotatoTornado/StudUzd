7cccb09194dd84dbcb15b99b05dcbd3146ea8ab6
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-03-04 22:12:11 +0200

    Update meniu.cpp

---
+++ meniu.cpp
@@ -0,0 +1,65 @@
+#include "meniu.h"
+#include "skaitymas.h"
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+
+
+void vykdytiMeniu(std::vector<Stud>& studis)
+{
+    Stud s;
+
+    while (true)
+    {
+        std::cout << "\nPasirinkite programos eiga:\n";
+        std::cout << "1 - Ivesti studenta ranka\n";
+        std::cout << "2 - Generuoti tik pazymius\n";
+        std::cout << "3 - Generuoti vardus, pavardes ir pazymius\n";
+        std::cout << "4 - Baigti darba\n";
+
+        int pasirinkimas;
+        std::cin >> pasirinkimas;
+
+        if (!std::cin)
+        {
+            throw std::runtime_error("Bloga ivestis.");
+        }
+
+        if (pasirinkimas == 4)
+        {
+            break;
+        }
+
+        if (pasirinkimas == 1)
+        {
+            skaitomRanka(s);
+        }
+        else if (pasirinkimas == 2)
+        {
+            std::cout << "Iveskite varda: ";
+            std::cin >> s.vard;
+            std::cout << "Iveskite pavarde: ";
+            std::cin >> s.pav;
+
+            parinktiAtsitiktinius(s);
+        }
+        else if (pasirinkimas == 3)
+        {
+            static int nr = 1;
+            s.vard = "Vardas" + std::to_string(nr);
+            s.pav = "Pavarde" + std::to_string(nr);
+            nr++;
+
+            parinktiAtsitiktinius(s);
+        }
+        else
+        {
+            std::cout << "Neteisingas pasirinkimas.\n";
+            continue;
+        }
+
+        studis.push_back(s);
+    }
+}

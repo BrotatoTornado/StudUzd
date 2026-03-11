14d3e99ae1c102a2018ca17b8bdd10ae14d6a587
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-03-11 23:11:52 +0200

    Update meniu.cpp

--- meniu.cpp
+++ meniu.cpp
@@ -1,11 +1,11 @@
 #include "meniu.h"
 #include "skaitymas.h"
+#include "Generavimas.h"
 
 #include <iostream>
 #include <stdexcept>
 #include <vector>
 #include <algorithm>
-
 
 void vykdytiMeniu(std::vector<Stud>& studis)
 {
@@ -18,7 +18,8 @@ void vykdytiMeniu(std::vector<Stud>& studis)
         std::cout << "2 - Skaityti is failo\n";
         std::cout << "3 - Generuoti tik pazymius\n";
         std::cout << "4 - Generuoti vardus, pavardes ir pazymius\n";
-        std::cout << "5 - Baigti darba\n";
+        std::cout << "5 - Sugeneruoti studentu faila\n";
+        std::cout << "6 - Baigti darba\n";
 
         int pasirinkimas;
         std::cin >> pasirinkimas;
@@ -28,43 +29,59 @@ void vykdytiMeniu(std::vector<Stud>& studis)
             throw std::runtime_error("Bloga ivestis.");
         }
 
-        if (pasirinkimas == 5)
+        if (pasirinkimas == 6)
         {
             break;
         }
 
-        if (pasirinkimas == 1)
+        switch (pasirinkimas)
         {
-            skaitomRanka(s);
-        }
-        else if (pasirinkimas == 2)
-        {
-            failoSkaitymas(studis);
-        }
-        else if (pasirinkimas == 3)
-        {
-            std::cout << "Iveskite varda: ";
-            std::cin >> s.vard;
-            std::cout << "Iveskite pavarde: ";
-            std::cin >> s.pav;
+            case 1:
+                skaitomRanka(s);
+                studis.push_back(s);
+                break;
+            case 2:
+                failoSkaitymas(studis);
+                studis.push_back(s);
+                break;
+            case 3:
+                std::cout << "Iveskite varda: ";
+                std::cin >> s.vard;
+                std::cout << "Iveskite pavarde: ";
+                std::cin >> s.pav;
 
-            parinktiAtsitiktinius(s);
-        }
-        else if (pasirinkimas == 4)
-        {
-            static int nr = 1;
-            s.vard = "Vardas" + std::to_string(nr);
-            s.pav = "Pavarde" + std::to_string(nr);
-            nr++;
+                parinktiAtsitiktinius(s);
 
-            parinktiAtsitiktinius(s);
-        }
-        else
-        {
-            std::cout << "Neteisingas pasirinkimas.\n";
-            continue;
-        }
+                studis.push_back(s);
 
-        studis.push_back(s);
+                break;
+            case 4:
+                static int nr = 1;
+                s.vard = "Vardas" + std::to_string(nr);
+                s.pav = "Pavarde" + std::to_string(nr);
+                nr++;
+
+                parinktiAtsitiktinius(s);
+
+                studis.push_back(s);
+
+                break;
+            case 5:
+                generuotiFaila();
+                char gr;
+                std::cout << "Grįžti į pradžią? (y/n): ";
+                std::cin >> gr;
+                if (gr == 'y' || gr == 'Y')
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            default:
+                std::cout << "Neteisingas pasirinkimas.\n";
+                continue;
+        }
     }
 }

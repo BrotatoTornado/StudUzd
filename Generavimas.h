d312c22d3f512ef91a73c1382c5b8b9d237b913a
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-03-12 01:40:02 +0200

    Update Generavimas.h

--- Generavimas.h
+++ Generavimas.h
@@ -1,4 +1,11 @@
 #pragma once
 #include <string>
+#include "studentas.h"
+
+void generuotiFaila();
+
+void skirstymasGrupes(std::vector<Stud>& studis);#pragma once
+#include <string>
+
 
 void generuotiFaila();

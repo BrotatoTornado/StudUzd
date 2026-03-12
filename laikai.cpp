76a377186564a9ef9875f75bf6fc03c9f319a04d
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-03-12 02:39:10 +0200

    Add files via upload

---
+++ laikai.cpp
@@ -0,0 +1,3 @@
+#include "laikai.h"
+
+TimerResults timers;
---
+++ laikai.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct TimerResults
+{
+    double generavimas = 0;
+    double skaitymas = 0;
+    double rusiavimas = 0;
+    double isvedimas = 0;
+};
+
+extern TimerResults timers;

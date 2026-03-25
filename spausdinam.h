faa2e8225b01d22abefe0c6ea6dc4896135e3082
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-03-25 23:44:17 +0200

    Update spausdinam.h

--- spausdinam.h
+++ spausdinam.h
@@ -1,10 +1,10 @@
 #pragma once
-#include "studentas.h"
-#include <vector>
-#include <string>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
 
-void spausdinam(std::vector<Stud>& studis, const std::string& pav);
-void rikiuotiStudentus(std::vector<Stud>& studis, const std::string& target);
+#include "konteineris.h"
+
+#include <string>
+
+void spausdinam(const StudContainer& studis, const std::string& pav);
+void rikiuotiStudentus(StudContainer& studis, const std::string& target);
+void surikiuotiStudentus(StudContainer& studis, int pasirinkimas);
+void surikiuotiPagalGalutiniVid(StudContainer& studis);

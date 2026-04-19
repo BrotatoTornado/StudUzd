d0f83a8b153e576722086b544d9f32270b5444c8
BrotatoTornado <mykoloasmeninis@gmail.com>
2026-04-19 21:49:51 +0300

    Update Generavimas.h

--- Generavimas.h
+++ Generavimas.h
@@ -7,13 +7,14 @@
 enum class SkirstymoStrategija
 {
     Pirma = 1,
-    Antra = 2
+    Antra = 2,
+    Trecia = 3
 };
 
 struct SkirstymoRezultatas
 {
     StudContainer vargsiukai;
-    StudContainer kietiakai;
+    StudContainer protai;
 };
 
 void generuotiFaila();
@@ -21,4 +22,5 @@ std::string failoPavadinimas(const std::string& bazinisPavadinimas);
 SkirstymoStrategija pasirinktiStrategija();
 SkirstymoRezultatas skirstymasStrategija1(const StudContainer& studis);
 SkirstymoRezultatas skirstymasStrategija2(StudContainer& studis);
+SkirstymoRezultatas skirstymasStrategija3(StudContainer& studis);
 SkirstymoRezultatas skirstymasGrupes(StudContainer& studis, SkirstymoStrategija strategija, bool irasytiIFailus = true, bool paprasytiRikiavimo = true);
